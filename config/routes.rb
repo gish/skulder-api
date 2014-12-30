dfbbd0fbc3f38b8d5efbbded8a752c2c416f70ad
Erik Hedberg <erik@gish.se>
2014-12-30 12:28:15 +0100

    Set correct routes for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
-      resources :users
+      get 'users', to: 'users#index'
+      get 'users/:id', to: 'users#show'
+      post 'users', to: 'users#create'
+      delete 'users', to: 'users#destroy'
+      put 'users', to: 'users#update'
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
