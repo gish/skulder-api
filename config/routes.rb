1a7ee32558b61336a29b8d601a36d546e3e4b263
Erik Hedberg <erik@gish.se>
2015-01-03 19:40:40 +0100

    Fix routing indentation

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
-      get 'users', to: 'users#index'
-      get 'users/:id', to: 'users#show'
-      post 'users', to: 'users#create'
-      delete 'users', to: 'users#destroy'
-      put 'users', to: 'users#update'
+    get 'users', to: 'users#index'
+    get 'users/:id', to: 'users#show'
+    post 'users', to: 'users#create'
+    delete 'users', to: 'users#destroy'
+    put 'users', to: 'users#update'
   end
 end
