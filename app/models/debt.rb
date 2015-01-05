877fe416b21317787f45c0e38c63d2d9b6f88014
Erik Hedberg <erik@gish.se>
2015-01-05 21:32:32 +0100

    Fix Debt model indentation

--- app/models/debt.rb
+++ app/models/debt.rb
@@ -1,15 +1,15 @@
 class Debt < ActiveRecord::Base
-    validates :amount, presence: true
-    validates :description, presence: true
-    validates :collector, presence: true
-    validates :loaner, presence: true
+  validates :amount, presence: true
+  validates :description, presence: true
+  validates :collector, presence: true
+  validates :loaner, presence: true
 
-    belongs_to :collector, :class_name => 'User'
-    belongs_to :loaner, :class_name => 'User'
+  belongs_to :collector, :class_name => 'User'
+  belongs_to :loaner, :class_name => 'User'
 
-    after_initialize :defaults
+  after_initialize :defaults
 
-    def defaults
-        self.uuid ||= SecureRandom.uuid
-    end
+  def defaults
+      self.uuid ||= SecureRandom.uuid
+  end
 end
