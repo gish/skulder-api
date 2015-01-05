f6f6e98a7dad2990ce83045ba4d5ce412ed5910c
Erik Hedberg <erik@gish.se>
2015-01-05 21:32:40 +0100

    Add validation of Debt creation params

--- app/controllers/api/debts_controller.rb
+++ app/controllers/api/debts_controller.rb
@@ -1,10 +1,45 @@
 class Api::DebtsController < ApplicationController
   def create
+    begin
+      params.require(:amount)
+      params.require(:description)
+      params.require(:loaner)
+      params.require(:collector)
+    rescue => e
+      render_error(
+        'missing_parameter',
+        "Missing parameter #{e.param}",
+        403
+      )
+      return
+    end
+
+    loaner = User.where(:uuid => params[:loaner]).take
+    collector = User.where(:uuid => params[:collector]).take
+
+    if not loaner
+      render_error(
+        'loaner_missing',
+        "Loaner #{params[:loaner]} doesn't exist",
+        403
+      )
+      return
+    end
+
+    if not collector
+      render_error(
+        'collector_missing',
+        "Collector #{params[:collector]} doesn't exist",
+        403
+      )
+      return
+    end
+
     debt = Debt.new(
       :amount => params[:amount],
       :description => params[:description],
-      :loaner => User.where(:uuid => params[:loaner]).take,
-      :collector => User.where(:uuid => params[:collector]).take
+      :loaner => loaner,
+      :collector => collector
     )
     debt.save
     render json: debt.as_json, status: 201, location: api_debt_url(:id => debt.uuid)
--- spec/controllers/api/debts_controller_spec.rb
+++ spec/controllers/api/debts_controller_spec.rb
@@ -4,27 +4,94 @@ RSpec.describe Api::DebtsController, :type => :controller do
   fixtures :users
 
   describe 'POST' do
-    it 'should create Debt when all parameters given' do
-      # given
-      debt = {
+    before(:each) do
+      @debt = {
         :amount => 1000,
         :description => 'Foo bar',
         :collector => users(:complete).uuid,
         :loaner => users(:complete).uuid
       }
+    end
+
+    it 'should create Debt when all parameters given' do
       # when
-      post :create, debt
+      post :create, @debt
       # then
       expect(response.status).to eql(201)
       expect(response.location).to eql(api_debt_url(:id => Debt.order(created_at: :desc).take.uuid))
     end
 
-    it 'should output error when amount missing'
-    it 'should output error when description missing'
-    it 'should output error when loaner missing'
-    it 'should output error when collector missing'
-    it 'should output error when loaner doesn\'t exist'
-    it 'should output error when collector doesn\'t exist'
+    it 'should output error when amount missing' do
+      # given
+      expected_type = 'missing_parameter'
+      @debt['amount'] = nil
+      # when
+      post :create, @debt
+      # then
+      message = JSON.parse(response.body)
+      expect(response.status).to eql(403)
+      expect(message['type']).to eql(expected_type)
+    end
+
+    it 'should output error when description missing' do
+      # given
+      expected_type = 'missing_parameter'
+      @debt['description'] = nil
+      # when
+      post :create, @debt
+      # then
+      message = JSON.parse(response.body)
+      expect(response.status).to eql(403)
+      expect(message['type']).to eql(expected_type)
+    end
+
+    it 'should output error when loaner missing' do
+      # given
+      expected_type = 'missing_parameter'
+      @debt['loaner'] = nil
+      # when
+      post :create, @debt
+      # then
+      message = JSON.parse(response.body)
+      expect(response.status).to eql(403)
+      expect(message['type']).to eql(expected_type)
+    end
+
+    it 'should output error when collector missing' do
+      # given
+      expected_type = 'missing_parameter'
+      @debt['collector'] = nil
+      # when
+      post :create, @debt
+      # then
+      message = JSON.parse(response.body)
+      expect(response.status).to eql(403)
+      expect(message['type']).to eql(expected_type)
+    end
+
+    it 'should output error when loaner doesn\'t exist' do
+      # given
+      expected_type = 'loaner_missing'
+      @debt['loaner'] = 'invalid'
+      # when
+      post :create, @debt
+      # then
+      message = JSON.parse(response.body)
+      expect(response.status).to eql(403)
+      expect(message['type']).to eql(expected_type)
+    end
+
+    it 'should output error when collector doesn\'t exist' do
+      # given
+      expected_type = 'collector_missing'
+      @debt['collector'] = 'invalid'
+      # when
+      post :create, @debt
+      # then
+      message = JSON.parse(response.body)
+      expect(response.status).to eql(403)
+      expect(message['type']).to eql(expected_type)
+    end
   end
 
   describe 'GET' do
